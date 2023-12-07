df9e095124803e39e539bee8d4ddbd90e42bf1c1
cryptojang <robert.crypto.jang@gmail.com>
2023-12-07 17:15:04 +0900

    metamask setting

--- src/pages/mint.jsx
+++ src/pages/mint.jsx
@@ -1,5 +1,33 @@
+import { useSDK } from "@metamask/sdk-react";
+import { useState } from "react";
+
 const Mint = () => {
-  return <div>mint</div>;
+  const [account, setAccount] = useState("");
+
+  const { sdk } = useSDK();
+
+  const onClickMetaMask = async () => {
+    try {
+      const accounts = await sdk?.connect();
+
+      setAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center">
+      {account ? (
+        <>
+          <div>{account}</div>
+          <button>mint</button>
+        </>
+      ) : (
+        <button onClick={onClickMetaMask}>MetaMask Login</button>
+      )}
+    </div>
+  );
 };
 
 export default Mint;
